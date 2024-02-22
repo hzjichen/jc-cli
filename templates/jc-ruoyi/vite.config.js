72fbc797157065af343d7edaaa76949913950233
liruiyang <17877111426@163.com>
2024-02-22 14:06:53 +0800

    feat:eslint prettierr配置

--- templates/jc-ruoyi/vite.config.js
+++ templates/jc-ruoyi/vite.config.js
@@ -1,58 +1,67 @@
-import { defineConfig, loadEnv } from 'vite'
-import path from 'path'
-import createVitePlugins from './vite/plugins'
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
+import createVitePlugins from './vite/plugins';
+import { visualizer } from 'rollup-plugin-visualizer';
+import eslintPlugin from 'vite-plugin-eslint';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
-  const env = loadEnv(mode, process.cwd())
-  const { VITE_APP_ENV } = env
-  return {
-    // 部署生产环境和开发环境下的URL。
-    // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
-    // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
-    base: VITE_APP_ENV === 'production' ? '/' : '/',
-    plugins: createVitePlugins(env, command === 'build'),
-    resolve: {
-      // https://cn.vitejs.dev/config/#resolve-alias
-      alias: {
-        // 设置路径
-        '~': path.resolve(__dirname, './'),
-        // 设置别名
-        '@': path.resolve(__dirname, './src')
-      },
-      // https://cn.vitejs.dev/config/#resolve-extensions
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-    },
-    // vite 相关配置
-    server: {
-      port: 90,
-      host: true,
-      open: true,
-      proxy: {
-        // https://cn.vitejs.dev/config/#server-proxy
-        '/dev-api': {
-          target: 'http://vue.ruoyi.vip/prod-api/',
-          changeOrigin: true,
-          rewrite: (p) => p.replace(/^\/dev-api/, '')
-        }
-      }
-    },
-    //fix:error:stdin>:7356:1: warning: "@charset" must be the first rule in the file
-    css: {
-      postcss: {
-        plugins: [
-          {
-            postcssPlugin: 'internal:charset-removal',
-            AtRule: {
-              charset: (atRule) => {
-                if (atRule.name === 'charset') {
-                  atRule.remove();
-                }
-              }
-            }
-          }
-        ]
-      }
-    }
-  }
-})
+	const env = loadEnv(mode, process.cwd());
+	const { VITE_APP_ENV } = env;
+	const plugins = [
+		createVitePlugins(env, command === 'build'),
+		mode === 'analysis' && visualizer({ open: true, filename: 'dist/analysis.html' }),
+		eslintPlugin({
+			include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
+		}),
+	];
+	return {
+		base: VITE_APP_ENV === 'production' ? '/' : '/',
+		plugins,
+		resolve: {
+			alias: {
+				// 设置路径
+				'~': path.resolve(__dirname, './'),
+				// 设置别名
+				'@': path.resolve(__dirname, './src'),
+				// i18n打包问题
+				'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+			},
+			extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+		},
+		// vite 相关配置
+		server: {
+			port: 90,
+			host: true,
+			open: true,
+			proxy: {
+				// https://cn.vitejs.dev/config/#server-proxy
+				'/dev-api': {
+					target: 'http://vue.ruoyi.vip/prod-api/',
+					changeOrigin: true,
+					rewrite: (p) => p.replace(/^\/dev-api/, ''),
+				},
+			},
+		},
+		build: {
+			chunkSizeWarningLimit: 1600,
+			target: 'esnext',
+			outDir: '../../dist/',
+		},
+		css: {
+			postcss: {
+				plugins: [
+					{
+						postcssPlugin: 'internal:charset-removal',
+						AtRule: {
+							charset: (atRule) => {
+								if (atRule.name === 'charset') {
+									atRule.remove();
+								}
+							},
+						},
+					},
+				],
+			},
+		},
+	};
+});
--- templates/jc-ruoyi/vite/plugins/auto-import.js
+++ templates/jc-ruoyi/vite/plugins/auto-import.js
@@ -1,12 +1,18 @@
-import autoImport from 'unplugin-auto-import/vite'
+import autoImport from "unplugin-auto-import/vite";
 
 export default function createAutoImport() {
-    return autoImport({
-        imports: [
-            'vue',
-            'vue-router',
-            'pinia'
-        ],
-        dts: false
-    })
+	return autoImport({
+		// 注册并使用配置项
+		include: [
+			/\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+			/\.vue$/,
+			/\.vue\?vue/, // .vue
+			/\.md$/, // .md
+		],
+		imports: ["vue", "vue-router", "pinia"],
+		dts: "src/auto-import.d.ts", // 路径下自动生成文件夹存放全局指令
+		eslintrc: {
+			enabled: true,
+		},
+	});
 }
